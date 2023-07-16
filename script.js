906cd2bf4191f43aa307f4e130b82ebb032c62d7
Sagar kumar <98587154+Sagarkumar2004@users.noreply.github.com>
2023-07-16 23:44:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 console.log("javascript is running");
 
+window.onload = function(){
+    document.querySelector("#loading").style.display = "none";
+    document.getElementById('main').style.opacity ="1"
+  }
 
 document.querySelector(".ham").addEventListener("click", ()=>{
     document.querySelector(".nav").classList.toggle("h-nav");
@@ -11,5 +15,4 @@ document.querySelector(".navlist").addEventListener("click", ()=>{
     document.querySelector(".navlist").classList.toggle("v-class");
     
 })
-
 
